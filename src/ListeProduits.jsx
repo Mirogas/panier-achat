80ed8baa8483ee7afdfc19b2e2f11a0d5568858e
Mirogas <clement.cj@outlook.com>
2022-02-09 14:39:39 -0500

    les éléments produits de la page sont maintenant dynamiques

--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -1,22 +1,50 @@
 import "./ListeProduits.css";
+import Produit from "./Produit.jsx";
+import lesProduits from "./data/produits.json";
 
 export default function articlesteProduits(){
-    let a = 5;
-    let b = a^4;
+    // console.log("la var lesProduits : ", lesProduits);
+
+    
+
+    // // methode #1 : prog imperative avec un boucle for********************************************
+    // let composantsProduits = [];
+    // // parcourir le tableau lesProduits et générer un composant produit pour chq élément
+    // for(let i=0; i<lesProduits.length; i++){
+    //     composantsProduits.push(<Produit nom={lesProduits[i].nom} prix={lesProduits[i].prix} pid={lesProduits[i].id}/>);
+    // }
+    // // ensuite mettre le tableau composantsProduits dans la div
+
+    // méthode #2 : exemple fonction avec map()************************************************************
+    let notes = [32.3, 44.67, 87.76, 76.88];
+
+    // map retourne un nouveau tableau avec chq élément de l'ancien tableau qui a été transformé sous le nom "uneNote" 
+    let notesArrondies = notes.map(uneNote => uneNote.toFixed()-0);
+
+    // est équivalent a cette fonction:
+    // let notesArrondies = notes.map(function(uneNote){
+    //     return uneNote.toFixed();
+    // });
+
+    console.log("le tableau des notes arrondies : ", notesArrondies);
+
     return(
         <section className="produits">
             <h2>Nos produits</h2>
             <div>
-                <article>
-                    <img src="" alt="" />
-                    <div className="titre">Titre du produit</div>
-                    <div className="prix">149.99$</div>
-                    <button>Ajouter au panier</button>
-                </article>
-                <article>Produit 2</article>
-                <article>Produit 3</article>
-                <article>Produit 4</article>
-                <article>Produit 5</article>
+
+                {/* retourne un tableau qui contient des composants produits avec les paramètres importés du produits.json(lesProduits) */}
+                {
+                    lesProduits.map(produit => <Produit nom={produit.nom} prix={produit.prix} pid={produit.id}/>)
+                }
+
+                {/* mettre un tableau qui contient des composants comme <Produit/>, ils sont automatiquement affichés dans le html */}
+                {/* {composantsProduits} */}
+
+
+                {/* <Produit nom="Chandail beige" prix="16.50" pid="prd0001"/>
+                <Produit nom="Chandail bleu rayé" prix="14.99" pid="prd0002"/>
+                <Produit nom="Chandail bleu" prix="12.25" pid="prd0003"/> */}
             </div>
         </section>
     )
--- src/Produit.jsx
+++ src/Produit.jsx
@@ -1,5 +1,12 @@
 import './Produit.css';
 
-export default function Produit(){
-
+export default function Produit(props){
+    return(
+        <article className="Produit">
+            <img src={"images-produits/"+ props.pid + ".webp"} alt={props.nom} />
+            <div className="titre">{props.nom}</div>
+            <div className="prix">{props.prix}</div>
+            <button>Ajouter au panier</button>
+        </article>
+    )
 }
