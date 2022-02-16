34d159f3297fa36b954691f26650303a6bba94cf
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2022-02-16 15:02:24 -0500

    Introduction de l'état du panier

--- src/Entete.jsx
+++ src/Entete.jsx
@@ -2,12 +2,14 @@ import './Entete.scss';
 import ShoppingCartSharpIcon from '@mui/icons-material/ShoppingCartSharp';
 import Badge from '@mui/material/Badge';
 
-export default function Entete() {
+export default function Entete(props) {
+    console.log("Les props du composant Entête : ", props);
+    console.log("Le panier dans Entête : ", props.panier);
     return (
         <header className="Entete">
             <h1>Magasin général</h1>
             <nav>
-                <Badge badgeContent={17} color="secondary">
+                <Badge badgeContent={Object.values(props.panier).length} color="secondary">
                     <ShoppingCartSharpIcon/>
                 </Badge>
                 <a href="#">Contactez-nous</a>
