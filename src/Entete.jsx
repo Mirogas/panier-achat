0a20765205e3da0477daf5eb326e68c5b467d1e7
Mirogas <clement.cj@outlook.com>
2022-02-23 15:19:49 -0500

    actualisation du badge du panier

--- src/Entete.jsx
+++ src/Entete.jsx
@@ -2,17 +2,19 @@ import './Entete.scss';
 import ShoppingCartSharpIcon from '@mui/icons-material/ShoppingCartSharp';
 import Badge from '@mui/material/Badge';
 
-export default function Entete(props) {
+export default function Entete({panier}) {
 
     // props contient l'objet panier ou sont stockés les items du panier de l'utilisateur
     // console.log(props);
+
+    let nbTotalQte = Object.values(panier).reduce((acc, article) => acc+article.qte, 0);
 
     return (
         <header className="Entete">
             <h1>Magasin général</h1>
             <nav>
                 <a href="#">
-                    <Badge badgeContent={Object.values(props.panier).length} color="primary">
+                    <Badge badgeContent={nbTotalQte} color="primary">
                         <ShoppingCartSharpIcon/>
                     </Badge>
                 </a>
